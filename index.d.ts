eb797c68e64c86d0feda41d1043a94a6e859429e
Erik Golinelli <erik@codekraft.it>
2024-04-11 11:27:09 +0200

    forces strict type checking

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ export declare module 'encoding' {
 
 export interface Compiler {
     _table: GetTextTranslations;
-    compile(): Buffer;
+    compile(): Buffer | string | undefined | null;
 }
 
 export interface GetTextComment {
@@ -22,20 +22,35 @@ export interface GetTextComment {
 export interface GetTextTranslation {
     msgctxt?: string;
     msgid: string;
-    msgid_plural?: string[];
+    msgid_plural?: string[] | string;
     msgstr: string[];
     comments?: GetTextComment;
 }
 
-export interface GetTextTranslations {
-    charset: string;
-    headers: { [headerName: string]: string };
-    translations: { [msgctxt: string]: { [msgId: string]: GetTextTranslation } };
+export interface GetTextTranslationRaw extends GetTextTranslation {
+    value: string;
+    type: number;
+    key: string;
+    obsolete?: boolean;
+    lastNode?: boolean;
 }
 
+export interface GetTextTranslations {
+    obsolete?: boolean | { [msgctxt: string]: { [msgId: string]: GetTextTranslation } };
+    charset?: string;
+    headers?: { [headerName: string]: string };
+    translations: gettextTranslation;
+}
+
+export type gettextTranslation = { [msgctxt: string]: { [msgId: string]: GetTextTranslation } }
+
 export interface parserOptions {
+    eol?: string;
     defaultCharset?: string;
     validation?: boolean;
+    foldLength?: string;
+    escapeCharacters?: string;
+    sort?: boolean;
 }
 
 export interface po {
